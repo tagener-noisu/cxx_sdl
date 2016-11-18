e478265659bb85608e72db67e8d15031b39cf15a
tagener-noisu <op4.renegat@gmail.com>
2016-11-18 16:34:04 +0600

    Create wrapper for SDL_Init routine

---
+++ include/sdl_wrapper.h
@@ -0,0 +1,37 @@
+#ifndef SDL_WRAPPER_H
+#define SDL_WRAPPER_H 1
+//-------------------------------------------------------------------
+#include <SDL.h>
+#include "wrapper/exceptions.h"
+//-------------------------------------------------------------------
+
+namespace SDL {
+
+template<class ErrorHandler =Throw>
+class SDL {
+	ErrorHandler handle_error();
+public:
+	SDL(Uint32 flags =0) {
+		if (SDL_Init(flags) != 0) handle_error(SDL_GetError());
+	}
+
+	~SDL() {
+		SDL_Quit();
+	}
+};
+
+template<>
+class SDL<NoThrow> {
+	int st;
+public:
+	SDL(Uint32 flags =0) :st {SDL_Init(flags)} {}
+
+	int state() const { return st; }
+
+	~SDL() {
+		SDL_Quit();
+	}
+};
+} //namespace
+//-------------------------------------------------------------------
+#endif
